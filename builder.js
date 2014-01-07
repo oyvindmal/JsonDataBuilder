4882fac6a83c7351f85c366515fa18f4fb96f48b
Oyvind Malin <oyvindmal@outlook.com>
2014-01-07 05:34:37 +0100

    Added support for passing title to modal

--- builder.js
+++ builder.js
@@ -14,9 +14,9 @@ $(document).ready(
 			$("#output").text(JSON.stringify(temp));
 		});
 		
-		$("#modal").click(function () { showModal("lol", function () { alert("Modal is live");  });});
+		$("#modal").click(function () { showModal("What is Lorep Ipsum?", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", function () { alert("Modal is live");  });});
 
-		function showModal(stringdata, callback)
+		function showModal(title, stringdata, callback)
 		{
 			var modalBackground = $("<div />");
 			modalBackground.addClass("modalBackground");
@@ -30,10 +30,13 @@ $(document).ready(
 			{
 				$(".modalBackground").remove();
 			});
-
+			var heading = $("<h3 />");
+			heading.text(title);
 			var content = $("<div />");
+			content.addClass("modalContent");
 			content.html(stringdata);
 			btn.appendTo(modal);
+			heading.appendTo(modal);
 			content.appendTo(modal);
 			modal.appendTo(modalBackground);
 			modalBackground.appendTo("body");
